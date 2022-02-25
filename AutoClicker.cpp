178fb4bc452253d19ffe3f453434cbdce2e55ee3
Quentin <74189637+QuentinHamann@users.noreply.github.com>
2022-02-25 14:58:28 +0100

    fonction drow et click

--- AutoClicker.cpp
+++ AutoClicker.cpp
@@ -2,61 +2,89 @@
 #include <iostream>
 #include "windows.h"
 
-/*
+
 using namespace std;
 
-int x, y;
-bool state(true);
-
-
-int draw() {
-    while (true) {
-        if (GetAsyncKeyState(VK_UP)) {
-            state = !state;
-            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
-        }
-
-        if (!state) {
-            x += 1;
-            SetCursorPos(x, y);
-        } else {
-            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
-        }
-        Sleep(10);
-
-    }
+AutoClicker::AutoClicker(int a, int b,int time, bool stat,std::string typeCLic) {
+    x = a;
+    y = b;
+    state = stat;
+    typeCLick = typeCLic;
+    duration = time;
 }
 
-int getTrajectory() {
+
+
+
+int AutoClicker::draw() {
+    int xpos [100] = {};
+    int ypos [100] = {};
+    int num = 0;
     while (true) {
         if (GetAsyncKeyState(VK_UP)) {
             POINT p;
             GetCursorPos(&p);
-            int mouseX = p.x;
-            int mouseY = p.y;
             cout << p.x << " " << p.y << endl;
+            xpos[num]=p.x;
+            ypos[num]=p.y;
+            num += 1;
+            cout << xpos << " " << ypos << endl;
+            Sleep(100);
         }
-        Sleep(100);
 
+        if (GetAsyncKeyState(VK_ESCAPE)) {
+            mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+            Sleep(100);
+            for (int i=0;i<num; i++) {
+                SetCursorPos(xpos[i], ypos[i]);
+                Sleep(100);
+            }
+            num = 0;
+        }
     }
 }
 
-int Set() {
+int AutoClicker::Set() {
     SetCursorPos(x, y);
 }
 
-int click() {
+void AutoClicker::config() {
+    cout << "Quel clic voulez-vous : gauche(g) ou droit(d) ? (par défaut : gauche) ";
+    cin >> typeCLick;
+    cout << endl << "Durée de l action :";
+    cin >> duration;
+}
 
+int AutoClicker::click() {
+    config();
     while (true) {
-        if (GetAsyncKeyState(VK_UP)) {
+        if (GetAsyncKeyState(VK_ESCAPE)) {
+            Sleep(100);
+            POINT p;
+            GetCursorPos(&p);
+            x = p.x;
+            y = p.y;
+        }
+
+        if (GetAsyncKeyState(VK_SPACE)) {
             state = !state;
         }
 
         if (!state) {
-            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
-            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            SetCursorPos(x, y);
+            if (typeCLick == "droit" or typeCLick =="d"){
+                mouse_event(MOUSEEVENTF_RIGHTDOWN, x, y, 0, 0);
+                mouse_event(MOUSEEVENTF_RIGHTUP, x, y, 0, 0);
+            }
+            else {
+                mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+                mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+            }
+            Sleep(duration);
+            break;
         }
-        cout << state << endl;
-        Sleep(50);
+
+
+
     }
-}*/
+}
--- AutoClicker.h
+++ AutoClicker.h
@@ -1,14 +1,21 @@
 #ifndef FINAL_AUTOCLICKER_H
 #define FINAL_AUTOCLICKER_H
-
+#import <iostream>
 
 class AutoClicker {
+private:
+    int x, y;
+    bool state = true;
+    int duration;
+    std::string typeCLick;
 
 public:
+    AutoClicker(int x, int y,int duration, bool state,std::string typeCLick);
+
     int draw();
-    int getTrajectory();
     int click();
     int Set();
+    void config();
 };
 
 
